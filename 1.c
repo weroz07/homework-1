a77b9939225a81bbd67d15d46996064ad2ac7a01
weroz07 <daniilparakhin2007@gmail.com>
2025-09-29 22:26:46 +0300

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,17 @@
+//#homevork 1
+#include<stdio.h>
+#include<locale.h>
+
+void main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	puts("       ___________           ");
+	puts("      |  __   ___ |          ");
+	puts("      | |  | |_|_||          ");
+	puts("      | |. | |_|_||          ");
+	puts("      | |__|      |          ");
+	puts("      |___________|           ");
+
+
+	return 0;
+}
